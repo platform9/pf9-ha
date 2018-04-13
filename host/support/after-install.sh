3170d4731953a7d27b17567374a386dbf48b31a4
Pushkar Acharya <pacharya@platform9.com>
2018-04-13 09:21:16 -0700

    Fix permissions on ha-slave directory

--- host/support/after-install.sh
+++ host/support/after-install.sh
@@ -6,6 +6,7 @@ chown -R pf9:pf9group /var/run/pf9-consul
 chown -R pf9:pf9group /var/run/pf9-ha
 chown -R pf9:pf9group /opt/pf9/consul-data-dir
 chown -R pf9:pf9group /opt/pf9/pf9-ha
+chown -R pf9:pf9group /opt/pf9/pf9-ha-slave
 chown -R pf9:pf9group /opt/pf9/etc/pf9-consul
 chown -R pf9:pf9group /opt/pf9/etc/pf9-ha
 chown -R pf9:pf9group /var/consul-status
