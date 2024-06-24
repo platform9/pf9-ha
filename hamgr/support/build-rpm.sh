e85cecdf93f679148adf2bf1673c5a1e30bba29a
Shweta chavan <shweta@platform9.com>
2024-06-24 12:09:53 +0530

    disable UTs, du-side rpm build

--- hamgr/support/build-rpm.sh
+++ hamgr/support/build-rpm.sh
@@ -8,13 +8,13 @@ BUILD_DIR=${REPO_DIR}/build
 
 BUILD_IMAGE=artifactory.platform9.horse/docker-local/py39-build-image:latest # can change to 5.6.0 if need be
 
-echo Building ha DU RPM ...
-docker run --rm -i \
-    -u $(id -u):$(id -g) \
-    -e PF9_VERSION=${PF9_VERSION:=1.3.0} \
-    -e BUILD_NUMBER=${BUILD_NUMBER:=0} \
-    -e GITHASH=${GITHASH} \
-    -v ${WORKSPACE}:/src \
-    -w /src/pf9-ha/hamgr/support \
-    ${BUILD_IMAGE} \
-    ./build.sh
+#echo Building ha DU RPM ...
+#docker run --rm -i \
+#    -u $(id -u):$(id -g) \
+#    -e PF9_VERSION=${PF9_VERSION:=1.3.0} \
+#    -e BUILD_NUMBER=${BUILD_NUMBER:=0} \
+#    -e GITHASH=${GITHASH} \
+#    -v ${WORKSPACE}:/src \
+#    -w /src/pf9-ha/hamgr/support \
+#    ${BUILD_IMAGE} \
+#    ./build.sh
--- pf9_test.sh
+++ pf9_test.sh
@@ -8,10 +8,10 @@ BUILD_DIR=${REPO_DIR}/build
 
 BUILD_IMAGE=artifactory.platform9.horse/docker-local/py39-build-image:latest # can change to 5.6.0 if need be
 
-echo Running tox unit tests ...
-docker run --rm -i \
-    -u $(id -u):$(id -g) \
-    -v ${WORKSPACE}:/src \
-    -w /src/pf9-ha \
-    ${BUILD_IMAGE} \
-    tox -v --recreate -e py39
+#echo Running tox unit tests ...
+#docker run --rm -i \
+#    -u $(id -u):$(id -g) \
+#    -v ${WORKSPACE}:/src \
+#    -w /src/pf9-ha \
+#    ${BUILD_IMAGE} \
+#    tox -v --recreate -e py39
